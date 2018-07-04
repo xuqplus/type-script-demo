1e8858a4a18c3a714179d1677e54ea89cb1b53a7
qunqun.xu <qunqun.xu@counect.com>
2018-07-04 10:41:40 +0800

    node import

--- src/day2/src/main.js
+++ src/day2/src/main.js
@@ -1,4 +1,4 @@
-function hello(compiler) {
-    console.log("Hello from " + compiler);
-}
-hello("TypeScript");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greet_1 = require("./greet");
+console.log(greet_1.sayHello("我是qq"));
